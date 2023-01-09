b6daf666455583d212b9b3f2236b458d4f4e2873
Marco <marcoastorga.g@gmail.com>
2023-01-09 16:26:03 -0500

    Initial Commit

---
+++ myFristProject/myFristProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFristProjectApp.swift
+//  myFristProject
+//
+//  Created by Marco Astorga González on 2023-01-09.
+//
+
+import SwiftUI
+
+@main
+struct myFristProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
