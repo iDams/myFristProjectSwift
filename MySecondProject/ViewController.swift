09421bcda6425f685c8281b4ea8d1c339007b125
Marco <marcoastorga.g@gmail.com>
2023-01-10 11:32:26 -0500

    init

---
+++ MySecondProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySecondProject
+//
+//  Created by Marco Astorga González on 2023-01-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
